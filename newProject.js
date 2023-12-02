e448fd40030b3d8add88007b381edbe927b0fa95
Shadow <togrul.huseynli.05@gmail.com>
2023-12-02 15:28:13 +0400

    project2

--- newProject.js
+++ newProject.js
@@ -1,5 +1,4 @@
-let numberOfFilms = +prompt('Neşə kinoya baxmısınız? ');
-
+let numberOfFilms = +prompt('Neçə kinoya baxmısınız? ');
 
 const personalMovieDb = {
     count: numberOfFilms,
@@ -8,10 +7,26 @@ const personalMovieDb = {
     generes: [],
     private: false
 };
+for (let i = 0; i < numberOfFilms; i++) {
+    let answer1 = prompt('Axırıncı baxdığınız film:');
+    let answer2 = +prompt('Bu kinonu neçə bal ilə qiymətləndirirsiz?');
+    if (answer1 == "" || answer2 == null || answer1 == null || answer2 == "" || answer1.length > 20) {
+        console.log("Səhv baş verdi.");
+        i--;
+    }
+    else {
+        personalMovieDb.movies[answer1] = answer2;
+    }
+}
 
-let answer1=prompt('Axiriinci baxdiginiz film');
-let answer2=+prompt('Nece bal ile qiynetlendirsiz?');
-  
-personalMovieDb.movies[answer1]=answer2;
+if (personalMovieDb.count < 10) {
+    console.log('Çox az filmə baxmısınız.');
+} else if (personalMovieDb.count < 30) {
+    console.log('Klassik film izləyicisi.');
+} else if (personalMovieDb.count > 30) {
+    console.log('Siz kinomansınız.');
+} else {
+    'Səhv baş verdi.'
+}
 
 console.log(personalMovieDb);
